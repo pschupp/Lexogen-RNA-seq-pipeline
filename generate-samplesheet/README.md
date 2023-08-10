35274f18dbfdaed4021cf4a1bd7a5a70c64bbe24
Patrick Schupp <patrick.schupp@ucsf.edu>
2023-08-10 13:10:48 -0700

    big update for samplesheet generator, supports variable barcode length and barcode schemes

--- generate-samplesheet/README.md
+++ generate-samplesheet/README.md
@@ -1,3 +1,49 @@
 # Usage
 
 
+Sample_Name,Sample_Plate,Sample_Well
+sample1,Set_A1,F4
+sample2,Set_A1,F5
+Sample_Name,Sample_Well_i7,Sample_Well_i5
+sample1,F4,A1
+sample2,F5,A1
+
+Attaching package: ‘renv’
+
+The following objects are masked from ‘package:base’:
+
+    autoload, load, remove
+
+Bioconductor version '3.14' is out-of-date; the current release version '3.17' is available with R version '4.3'; see
+  https://bioconductor.org/install
+
+Attaching package: ‘BiocManager’
+
+The following object is masked from ‘package:renv’:
+
+    install
+
+[1] 2
+Using BLAS/LAPACK: ATLAS
+[0;38;5;40mnormal[0m [38;2;203;75;22mx[x<=-0.01][0m [38;2;131;148;150mx[abs(x)<0.01][0m [38;2;181;137;0mx[x>=0.01][0m [38;2;211;54;130m19/01/2038 03:14:07[0m [38;2;133;153;0m"string"[0m
+[38;2;108;113;196mNA/NaN/NULL[0m [38;2;220;50;47mFALSE[0m [38;2;133;153;0mTRUE[0m [38;2;211;54;130mInf[0m [38;2;131;148;150m[index][0m [38;2;211;54;130mstderror[0m [38;2;181;137;0mwarn[0m [38;2;211;54;130merror[0m
+usage: ./generate-samplesheet.R [-h] [--rc] [--samplebarcs] [--workingdir]
+                                [--investigator] [--experiment] [--date]
+                                [--read1] [--read2] [--i7r] [--i5r]
+
+Generate Sample Sheets for sequencing of Lexogen FWD 3' Libraries. Supports
+both basic i5/i7 6nt indexing and the UDIs.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --rc , -c             reverse complement the i5 barcode
+  --samplebarcs , -s    location of csv containing columns: sample name,
+                        sample barcode plate, sample barcode well
+  --workingdir , -w     current working directory
+  --investigator , -i   name of the investigator, default is: 'anonymous'
+  --experiment , -e     experiment name, default is: 'UCSF experiment'
+  --date , -d           date of the run, default is today: '08/10/2023'
+  --read1 , -r          length of read 1, default is: '66'
+  --read2 , -u          length of read 2, default is: '0'
+  --i7r , -v            length of the first index read, i7
+  --i5r , -x            length of the second index read, i5
--- generate-samplesheet/generate-samplesheet.R
+++ generate-samplesheet/generate-samplesheet.R
@@ -1,24 +1,10 @@
 #!/usr/bin/env Rscript
+
 suppressPackageStartupMessages(library("argparse"))
 suppressPackageStartupMessages(library('xlsx'))
 
-# goal: automate production of sample shwet for sequencing and demultiplexing with minimal user input
-
-# input:
-# 1. sample-associated-barcode.csv - csv of sample name, sample barcode plate, and sample barcode well
-# 2. DONE - lexogen barcode sheet 
-
-# output: 
-# 1. Sample sheet with following columns:
-#       Sample_ID	Sample_Name	Sample_Plate	Sample_Well	I7_Index_ID	index	I5_Index_ID	index2	Sample_Project	Description
-# 2. Optional: full sample sheet with appropriate header, etc., but this is fairly trivial and be a later TODO
-########################
+# take arguments from command line
 parser = ArgumentParser(description = "Generate Sample Sheets for sequencing of Lexogen FWD 3' Libraries. Supports both basic i5/i7 6nt indexing and the UDIs.")
-
-parser$add_argument('--type', '-t', 
-                    type='character',
-                    metavar = '',
-                    help="barcode type from Lexogen, one of 'UDI' or 'standard'")
 
 parser$add_argument('--rc', '-c', 
                     type='logical',
@@ -76,29 +62,83 @@ parser$add_argument('--i5r', '-x',
                     metavar = '',
                     help = "length of the second index read, i5")
 
-args <- parser$parse_args()
-WD <- args$working_dir
+# process arguments and raise error if there are any with no values
+args = parser$parse_args()
 noArgs = unlist(lapply(args, is.null))
 if(any(noArgs)){
     stop(paste0('need arguments for options: ', paste0(names(args)[noArgs], collapse=', ')))
 }
-if(args$type == 'UDI'){
+
+# reassign arguments to more intuitive variable names
+WD = args$working_dir
+i7Cycles = args$i7r
+i5Cycles = args$i5r
+sampleSheet = read.csv(args$samplebarcs)
+input=read.csv(args$samplebarcs)
+if (any(colnames(input)== 'Sample_Well')){
+    typeLex = 'UDI'
+} else{
+    typeLex = 'standard'
+}
+
+# get the relevant barcode excel sheets for the UDI barcodes...
+if(typeLex == 'UDI'){
     barcodes = lapply(seq(1,5), function(i) 
         {read.xlsx('../lexogen-barcodes/107UI264V0104_UDI-12-nt-Index-Sequences-for-Illumina_2021-03-26.xlsx', sheetIndex=i)[1:96,seq(1,7)]}
     )
     names(barcodes)=c(paste0('Set_A', seq(1,4)), 'Set_B1')
+    outA = lapply(seq_along(input$Sample_Plate), function(i) {
+        inputT = input[i,]
+        plate=input$Sample_Plate[i]
+        barcI = grep(plate, names(barcodes))
+        joinedBarc=gsub(' ' , '', apply(barcodes[[barcI]][,c(1,2)], 1, function(x) paste0(x[1], x[2])))
+        out=cbind(i, inputT,
+            barcodes[[barcI]][match(inputT$Sample_Well,  joinedBarc),-seq(1,3)])
+        colnames(out) = c('Sample_ID','Sample_Name','Sample_Plate','Sample_Well','I7_Index_ID','index','I5_Index_ID','index2')
+        # process barcodes based on barcode length
+        if(i7Cycles != 12){
+            out$index = substr(out$index, 1, i7Cycles)
+        }
+        if(i5Cycles != 12){
+            out$index = substr(out$index, 1, i5Cycles)
+        }
+        return(out)
+    })
 }
-if(args$type == 'standard'){
+
+# and the standard 6nt barcodes...
+if(typeLex == 'standard'){
     barcodes = lapply(seq(1,3), function(i) 
         {read.xlsx('../lexogen-barcodes/044UI263V0100_i7-and-i5-nt-Index-Sequences-for-Illumina.xlsx', sheetIndex=i)[1:96,seq(1,4)]}
     )
     names(barcodes)=c('i7', 'i5', 'i5rc')
+    outA = lapply(seq_along(input$Sample_Name), function(i) {
+        inputT = input[i,]
+        joinedBarc=gsub(' ' , '', apply(barcodes[[1]][,c(1,2)], 1, function(x) paste0(x[1], x[2])))
+        if(args$rc == FALSE){
+            out=cbind(i, inputT,
+                barcodes[[1]][match(inputT$Sample_Well_i7,  joinedBarc),-seq(1,3)], 
+                barcodes[[2]][match(inputT$Sample_Well_i5,  joinedBarc),-seq(1,3)])
+            colnames(out) = c('Sample_ID','Sample_Name', 'Sample_Well_i7', 'Sample_Well_i5','index','index2')
+        } else if(args$rc == TRUE){
+            out=cbind(i, inputT,
+                barcodes[[1]][match(inputT$Sample_Well_i7,  joinedBarc),-seq(1,3)], 
+                barcodes[[3]][match(inputT$Sample_Well_i5,  joinedBarc),-seq(1,3)])
+            colnames(out) = c('Sample_ID','Sample_Name', 'Sample_Well_i7', 'Sample_Well_i5', 'index','index2')
+        }
+        # process barcodes based on barcode length
+        if(i7Cycles == 0){
+            out = out[,-c(3,5)]
+        }
+        if(i5Cycles == 0){
+            out = out[,-c(4,6)]
+        }
+        return(out)
+    })
 }
 
-sampleSheet = read.csv(args$samplebarcs)
-i7Cycles = args$i7r
-i5Cycles = args$i5r
-fileName = paste0('samplesheet_', args$experiment, '_', gsub('/', '-', format(Sys.time(), "%x")), '.csv')
+# write out the new sample sheet
+fileName = paste0('samplesheet_', gsub(' ', '-', args$experiment), '_', gsub('/', '-', format(Sys.time(), "%x")), '.csv')
 cat('[Header]\n', file = fileName)
 cat('FileFormatVersion,2\n', file = fileName, append = TRUE)
 cat(paste0('Investigator Name,', args$investigator, '\n'), file = fileName, append = TRUE)
@@ -111,22 +151,4 @@ cat(paste0('Read2Cycles,', args$read2, '\n'), file = fileName, append = TRUE)
 cat(paste0('Index1Cycles,', i7Cycles, '\n'), file = fileName, append = TRUE)
 cat(paste0('Index2Cycles,', i5Cycles, '\n\n'), file = fileName, append = TRUE)
 cat('[BCLConvert_Data]\n', file = fileName, append = TRUE)
-
-input=read.csv(args$samplebarcs)
-
-outA = lapply(seq_along(input$Sample_Plate), function(i) {
-    inputT = input[i,]
-    plate=input$Sample_Plate[i]
-    barcI = grep(plate, names(barcodes))
-    joinedBarc=gsub(' ' , '', apply(barcodes[[barcI]][,c(1,2)], 1, function(x) paste0(x[1], x[2])))
-    out=cbind(i, inputT, 
-        barcodes[[barcI]][match(inputT$Sample_Well,  joinedBarc),-seq(1,3)]) 
-    colnames(out) = c('Sample_ID','Sample_Name','Sample_Plate','Sample_Well','I7_Index_ID','index','I5_Index_ID','index2')
-    return(out)
-})
-
 write.table(do.call(rbind, outA), file = fileName, append = TRUE, quote = FALSE, row.names = FALSE, sep = ',')
-
-#outA = do.call(rbind, outA)
-#system('cp sample_top.txt new_sample_sheet.txt')
-#lapply(seq_len(nrow(outA)), function(x) write(paste0(outA[x,], collapse=','), file='new_sample_sheet.txt', append = TRUE))
